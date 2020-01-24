f8cfacc30ccf93d3934e5de9c25bb666ffa737ab
TMSBRR <48.wk.s1c99sx.tms@gmail.com>
2020-01-24 10:55:16 -0600

    fixed-img

--- YSite/src/app/lang/lang.component.ts
+++ YSite/src/app/lang/lang.component.ts
@@ -9,25 +9,25 @@ import {Idioma} from "../../assets/Idioma";
 export class LangComponent implements OnInit {
 			
   languages: Idioma []= [
-  {img:"../../assets/img/flags/ger.gif", nombre:"German"},
-  {img:"../../assets/img/flags/china.gif", nombre:"Chinese"},
-  {img:"../../assets/img/flags/rusia.gif", nombre:"Russian"},
-  {img:"../../assets/img/flags/italy.gif", nombre:"Italian"},
-  {img:"../../assets/img/flags/por.gif", nombre:"Portuguese"},
-  {img:"../../assets/img/flags/france.gif", nombre:"French"}
+  {img:"assets/img/flags/ger.gif", nombre:"German"},
+  {img:"assets/img/flags/china.gif", nombre:"Chinese"},
+  {img:"assets/img/flags/rusia.gif", nombre:"Russian"},
+  {img:"assets/img/flags/italy.gif", nombre:"Italian"},
+  {img:"assets/img/flags/por.gif", nombre:"Portuguese"},
+  {img:"assets/img/flags/france.gif", nombre:"French"}
   ];
 
 		 languages2: Idioma []= [
-  {img:"../../assets/img/flags/hungary.gif", nombre:"Hungarian"},
-  {img:"../../assets/img/flags/israel.gif", nombre:"Hebrew"},
-  {img:"../../assets/img/flags/japan.gif", nombre:"Japanese"},
-  {img:"../../assets/img/flags/fin.gif", nombre:"Finnish"},
-  {img:"../../assets/img/flags/greece.gif", nombre:"Greek"},
-  {img:"../../assets/img/flags/nor.gif", nombre:"Norge"},
-  {img:"../../assets/img/flags/iran.gif", nombre:"Farsi"},
-  {img:"../../assets/img/flags/ned.gif", nombre:"Nederlands"},
-  {img:"../../assets/img/flags/pol.gif", nombre:"Polish"},
-  {img:"../../assets/img/flags/turkey.gif", nombre:"Turkish"},
+  {img:"assets/img/flags/hungary.gif", nombre:"Hungarian"},
+  {img:"assets/img/flags/israel.gif", nombre:"Hebrew"},
+  {img:"assets/img/flags/japan.gif", nombre:"Japanese"},
+  {img:"assets/img/flags/fin.gif", nombre:"Finnish"},
+  {img:"assets/img/flags/greece.gif", nombre:"Greek"},
+  {img:"assets/img/flags/nor.gif", nombre:"Norge"},
+  {img:"assets/img/flags/iran.gif", nombre:"Farsi"},
+  {img:"assets/img/flags/ned.gif", nombre:"Nederlands"},
+  {img:"assets/img/flags/pol.gif", nombre:"Polish"},
+  {img:"assets/img/flags/turkey.gif", nombre:"Turkish"},
   ];
 
   constructor() {}
